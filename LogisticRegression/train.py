32e755002c62c66d153dee9913489b841f7e99c7
deepakhr1999 <deepakhr1999@gmail.com>
2021-03-11 19:28:04 +0530

    removed important lines to give blanks for assignment'

--- LogisticRegression/train.py
+++ LogisticRegression/train.py
@@ -1,11 +1,6 @@
-import os
-import pickle
-import argparse
+import os, pickle, argparse, pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
 from time import time
-import pandas as pd
 
 '''For ignoring useless warnings'''
 from warnings import simplefilter
@@ -23,22 +18,17 @@ the mnist module from the tensorflow.keras library
 """
 print('-----------------Data-----------------')
 print('Loading MNIST data...')
-train = pd.read_csv('train.csv')
+train, test = pd.read_csv('train.csv'), pd.read_csv('test.csv')
 X_train, y_train = train.values[:, :-1], train.values[:, -1]
-
-test = pd.read_csv('test.csv')
 X_test, y_test = test.values[:, :-1], test.values[:, -1]
 
 # normalize images such that pixels are in range [0.0,1.0]
-numTrain = len(X_train)
-X_train = X_train / 255
+numTrain, numTest = len(X_train), len(X_test)
+X_train, X_test = X_train / 255, X_test / 255
+
 print(f'Number of training examples: {numTrain}')
 print(f'Training examples shape :', X_train.shape)
 print()
-
-
-numTest = len(X_test)
-X_test  = X_test / 255
 print(f'Number of testing examples: {numTest}')
 print(f'Testing examples shape: ', X_test.shape)
 print()
@@ -48,10 +38,12 @@ print()
 Section 1: DECLARE your model here!
 The class LogisticRegression has already been imported from
 sklearn.linear_model module.
+Declare a logistic regression object and store it
+in the variable 'model'
 """
 print('-----------------Model-----------------')
 ### 1. Your code starts here
-model = LogisticRegression()
+
 ### 1. You code ends here
 print(model)
 print()
@@ -65,14 +57,25 @@ print('Training model :')
 print('(This takes around a minute, depending on your cpu speed)')
 start_time = time()
 ### 2. Your code starts here
-model.fit(X_train, y_train)
+
 ### 2. You code ends here
 train_duration = time() - start_time
 print(f'Success: Finished training model in {train_duration} seconds')
 print()
 
+"""
+Section 3: TESTING model
+We predict our answer on test data and compare our predictions with
+the actual test labels. We then display our accuracy on the test data.
+Use the model.predict method and pass X_test as argument.
+Store the value in the variable test_predictions
+"""
 print('Testing model...', end=' ')
-acc = model.score(X_test, y_test)
+### 3. Your code starts here
+
+### 3. Your code ends here
+
+acc = (test_predictions == y_test).mean()
 print(f'accuracy on unseen data: {acc*100}% !')
 print()
 
